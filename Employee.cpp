3186ec04950343a1ddf90e656a36491f51980299
md33517 <148026091+md33517@users.noreply.github.com>
2023-10-15 22:13:20 -0500

    Create Employee.cpp

---
+++ Employee.cpp
@@ -0,0 +1,18 @@
+#include "Employee.h"
+
+Employee::Employee(string theName, double theWage, double theHours)
+{
+    name = theName;
+    wage = theWage;
+    hours = theHours;
+}
+
+double Employee::calcPay() const
+{
+    return wage * hours;
+}
+
+string Employee::getName() const
+{
+    return name;
+}
